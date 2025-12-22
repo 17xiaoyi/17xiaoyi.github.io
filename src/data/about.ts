b08b03238b24a2e5e1dcf7e53cb5c63a4d7a8254
17xiaoyi <yiqih777@mit.edu>
2025-12-22 07:14:53 -0500

    Update about.ts

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,11 +1,11 @@
 export const aboutMarkdown = `# Research
 
-- On the rate of convergence of cylindrical singularity in mean curvature flow (with [X. Zhao](https://sites.google.com/view/xinrui-zhao/home?authuser=0)), [arXiv:2510.23499](https://arxiv.org/abs/2510.23499).
-- The bounded diameter conjecture and sharp geometric estimates for mean curvature flow,(with [W. Jiang](https://person.zju.edu.cn/en/wsjiang)), [arXiv:2510.17060](https://arxiv.org/abs/2510.17060).
-- The nodal sets of solutions to parabolic equations (with [W. Jiang](https://person.zju.edu.cn/en/wsjiang)), [arXiv:2406.05877](https://arxiv.org/abs/2406.05877).
-- Volume estimates on singular sets and critical sets for elliptic equations with Hölder coefficients (with [W. Jiang](https://person.zju.edu.cn/en/wsjiang)), [arXiv:2309.08089](https://arxiv.org/abs/2309.08089).
-- Scalar curvature lower bound under integral convergence (with [M.-C. Lee](https://sites.google.com/view/mcleemath/home)), ***[Math. Z.](https://link.springer.com/article/10.1007/s00209-022-03155-9)***, 303 (2023), no. 1, Paper No. 2. 53, [arXiv:2111.05079](https://arxiv.org/abs/2111.05079).
-- Spectral analysis beyond ℓ² on Sierpinski Lattices (with [S. Cao](https://sites.google.com/view/shipingcao), [R. Strichartz](https://en.wikipedia.org/wiki/Robert_Strichartz), [H. Qiu](https://math.nju.edu.cn/~huaqiu/) and [X. Zhu](https://scholar.google.com/citations?user=IsTgOccAAAAJ&hl=en)), [***J Fourier Anal Appl***](https://link.springer.com/article/10.1007/s00041-021-09853-y) 27, 55 (2021), [arXiv:1910.01771](https://arxiv.org/abs/1910.01771).
+- *On the rate of convergence of cylindrical singularity in mean curvature flow* (with [X. Zhao](https://sites.google.com/view/xinrui-zhao/home?authuser=0)), [arXiv:2510.23499](https://arxiv.org/abs/2510.23499).
+- *The bounded diameter conjecture and sharp geometric estimates for mean curvature flow* (with [W. Jiang](https://person.zju.edu.cn/en/wsjiang)), [arXiv:2510.17060](https://arxiv.org/abs/2510.17060).
+- *The nodal sets of solutions to parabolic equations* (with [W. Jiang](https://person.zju.edu.cn/en/wsjiang)), [arXiv:2406.05877](https://arxiv.org/abs/2406.05877).
+- *Volume estimates on singular sets and critical sets for elliptic equations with Hölder coefficients* (with [W. Jiang](https://person.zju.edu.cn/en/wsjiang)), [arXiv:2309.08089](https://arxiv.org/abs/2309.08089).
+- *Scalar curvature lower bound under integral convergence* (with [M.-C. Lee](https://sites.google.com/view/mcleemath/home)), ***[Math. Z.](https://link.springer.com/article/10.1007/s00209-022-03155-9)***, 303 (2023), no. 1, Paper No. 2. 53, [arXiv:2111.05079](https://arxiv.org/abs/2111.05079).
+- *Spectral analysis beyond ℓ² on Sierpinski Lattices* (with [S. Cao](https://sites.google.com/view/shipingcao), [R. Strichartz](https://en.wikipedia.org/wiki/Robert_Strichartz), [H. Qiu](https://math.nju.edu.cn/~huaqiu/) and [X. Zhu](https://scholar.google.com/citations?user=IsTgOccAAAAJ&hl=en)), [***J Fourier Anal Appl***](https://link.springer.com/article/10.1007/s00041-021-09853-y) 27, 55 (2021), [arXiv:1910.01771](https://arxiv.org/abs/1910.01771).
 
 # Selected honours
 
