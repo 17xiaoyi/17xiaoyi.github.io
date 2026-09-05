d286f38f1880c02d5d36dac3693c2098728bcc53
17xiaoyi <yiqih777@mit.edu>
2026-09-05 01:06:13 -0400

    Update about.ts

--- src/data/about.ts
+++ src/data/about.ts
@@ -8,12 +8,14 @@ export const aboutMarkdown = `# Research
 - *Scalar curvature lower bound under integral convergence* (with [M.-C. Lee](https://sites.google.com/view/mcleemath/home)), ***[Math. Z.](https://link.springer.com/article/10.1007/s00209-022-03155-9)***, 303 (2023), no. 1, Paper No. 2. 53, [arXiv:2111.05079](https://arxiv.org/abs/2111.05079).
 - *Spectral analysis beyond ℓ² on Sierpinski Lattices* (with [S. Cao](https://sites.google.com/view/shipingcao), [R. Strichartz](https://en.wikipedia.org/wiki/Robert_Strichartz), [H. Qiu](https://math.nju.edu.cn/~huaqiu/) and X. Zhu), [***J. Fourier Anal. Appl.***](https://link.springer.com/article/10.1007/s00041-021-09853-y) 27 (2021), no. 3, Paper No. 55, [arXiv:1910.01771](https://arxiv.org/abs/1910.01771).
 
-# Selected honours
+# Honours
 
+- Charles W. and Jennifer C. Johnson Prize, MIT, 2026
+- Mathworks Fellowship, 2026
 - Simons Dissertation Fellowship, Simons Foundation, 2025
 - Croucher Scholarship, Croucher Foundation, 2024
-- Mathworks Fellowship, MIT, 2023
-- Peterson Fellowship, MIT, 2022
+- Mathworks Fellowship, 2023
+- Frank L. Peterson Fellowship, MIT, 2022
 - Best Teaching Assistant Award, CUHK, 2022
-- Q.W. Lee Scholarship, CUHK, 2019
-`;
+- Q.W. Lee Scholarship, CUHK, 2020
+;
